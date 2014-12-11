fb5e69ef1a28601b0ef1400b2ca47674ab6fc520
Giuseppe DiBella <gd46@njit.edu>
2014-12-10 21:57:28 -0500

    Added place holder for other pages

--- index.php
+++ index.php
@@ -29,9 +29,8 @@
         </div>
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li><a href="#about">Search by String</a></li>
+            <li><a href="#about">Search by User</a></li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
