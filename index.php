dc2d5a5e0777600f3821143cc5ffb9ebf2d04951
Giuseppe DiBella <gd46@njit.edu>
2014-12-16 12:13:59 -0500

    Added link to elasticsearch.min.js

--- index.php
+++ index.php
@@ -17,6 +17,7 @@
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+    <script src="js/elasticsearch.min.js"></script>
 
 	</head>
 	<body>
