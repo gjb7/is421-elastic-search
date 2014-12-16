307761be8623688abaa0d4ea74fbd9405bd659d9
Grant J. Butler <butler.grant.j@gmail.com>
2014-12-16 12:49:56 -0500

    Add javascript handling for searching.

---
+++ js/tweets.js
@@ -0,0 +1,41 @@
+(function() {
+  var client = new elasticsearch.Client({
+    host: 'localhost:9200'
+  });
+  
+  var searchField = $('#search');
+  var searchUserField = $('#user');
+  var queryFormat = 'text:"* {QUERY} *" OR "{QUERY} *" OR "* {QUERY}"';
+  
+  $('#submit').click(function() {
+    var searchString = searchField.val().trim();
+    var userSearchString = searchUserField.length ? searchField.val().trim() : null;
+    
+    if (searchString.length <= 0) {
+      alert('Please enter a valid search string.');
+      
+      return;
+    }
+    
+    if (userSearchString && userSearchString.length <= 0) {
+      alert('Please enter a user to search for.');
+      
+      return;
+    }
+    
+    
+    var query = queryFormat.replace(/{QUERY}/ig, searchString);
+    
+    if (userSearchString) {
+      query = '(' + query + ') AND user.username:"' + userSearchString + '"';
+    }
+    
+    client.search({
+      index: 'twitter',
+      type: 'tweets',
+      q: query
+    }, function(error, resposne) {
+      console.log(arguments);
+    });
+  });
+})();
